8eeff60fd4c05e9f923b123b1e6e109b7b47da5f
dustin <nolaxking@gmail.com>
2022-04-10 10:44:14 -0700

    first commit

--- Project-_WeLoveMovies-_Qualified-Dustin_Dampeer-Solution/src/movies/moives.service.js
+++ Project-_WeLoveMovies-_Qualified-Dustin_Dampeer-Solution/src/movies/moives.service.js
@@ -8,7 +8,7 @@ function isShowing(is_showing) {
   return knex("movies as m ")
     .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
     .select("m.*", "mt.is_showing")
-    .groupBy("mt.movie_id")
+    
     .where({ is_showing: is_showing });
 }
 
